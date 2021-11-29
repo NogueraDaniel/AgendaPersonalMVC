647d1e64cf51e63919abe867e532c89b56635ef6
NogueraDaniel <noguera.daniel88@gmail.com>
2021-11-28 21:35:37 -0300

    Agrego estilos, listo en orden alfabetico

--- AgendaPersonal/Controllers/HomeController.cs
+++ AgendaPersonal/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using AgendaPersonal.Models;
+﻿using AgendaPersonal.Context;
+using AgendaPersonal.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -10,16 +12,39 @@ using System.Threading.Tasks;
 namespace AgendaPersonal.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+    {
+
+
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly AgendaDatabaseContext _dbContext;
+
+
+
+        public HomeController(ILogger<HomeController> logger, AgendaDatabaseContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
+
         }
 
         public IActionResult Index()
         {
+
+            //Obtengo un arreglo de los contactos en la base de datos
+
+            List<Contacto> contactos = new List<Contacto>();
+
+            foreach (var contacto in _dbContext.Contactos)
+            {
+                contactos.Add(contacto);
+            }
+            //List<Contacto> contactos = (from Contacto in _dbContext.Contactos  );
+
+            List<Contacto> _contactos = contactos.OrderBy(x => x.Nombre).ToList();
+
+            ViewData["_contactos"] = _contactos;
             return View();
         }
 
@@ -33,5 +58,6 @@ namespace AgendaPersonal.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
--- AgendaPersonal/Models/Contacto.cs
+++ AgendaPersonal/Models/Contacto.cs
@@ -9,19 +9,23 @@ namespace AgendaPersonal.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int idContacto { get; set; }
 
-
+        [Required(ErrorMessage = "Ingrese un nombre válido")]
         public string Nombre { get; set; }
 
-
+        [Required(ErrorMessage = "Ingrese un apellido válido")]
         public string Apellido { get; set; }
 
-
+        [Required(ErrorMessage = "Ingrese un email válido")]
+        [EmailAddress(ErrorMessage = "Ingrese un email válido")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Ingrese un telefono válido")]
         public int Telefono { get; set; }
 
+        [Required(ErrorMessage = "Ingrese una direccion válida")]
         public string Direccion { get; set; }
 
+        [Required(ErrorMessage = "Defina el tipo de contacto")]
         [EnumDataType(typeof(Tipo))]
         [Display(Name = "Tipo de contacto")]
         public Tipo tipoContacto { get; set; }
